d06cd55408661290787a1f9aead3d9447263b7d6
parulpragya767 <parulpragya767@gmail.com>
2026-02-13 10:19:59 +0530

    using testcontainers for postgress testing

---
+++ src/test/java/com/lovingapp/AbstractIntegrationTest.java
@@ -0,0 +1,29 @@
+package com.lovingapp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("test")
+public abstract class AbstractIntegrationTest {
+
+    @Container
+    @SuppressWarnings("resource")
+    private static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("loving_test")
+            .withUsername("postgres")
+            .withPassword("postgres");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES::getUsername);
+        registry.add("spring.datasource.password", POSTGRES::getPassword);
+    }
+}
--- src/test/java/com/lovingapp/LovingApplicationTests.java
+++ src/test/java/com/lovingapp/LovingApplicationTests.java
@@ -1,12 +1,8 @@
 package com.lovingapp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
-class LovingApplicationTests {
+class LovingApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
